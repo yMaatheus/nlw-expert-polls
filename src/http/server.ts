889f7cc793029834aefea193eeec7e7b09a610bf
Matheus <ymatheus.h@gmail.com>
2024-02-16 19:34:39 -0300

    feat: :sparkles: implements fastify cookie

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,7 +1,13 @@
+import cookie from "@fastify/cookie";
 import fastify from "fastify";
 import { createPoll } from "./routes/create-poll";
 
 const app = fastify();
+
+app.register(cookie, {
+  secret: "polls-app-nlw",
+  hook: 'onRequest',
+})
 
 app.register(createPoll);
 
