2a4be10b5fa603e4340b5297ab4f8d35ec9e7c9d
Matheus <ymatheus.h@gmail.com>
2024-02-16 20:10:48 -0300

    feat: :sparkles: create route vote on poll

--- src/http/server.ts
+++ src/http/server.ts
@@ -2,6 +2,7 @@ import cookie from "@fastify/cookie";
 import fastify from "fastify";
 import { createPoll } from "./routes/create-poll";
 import { getPoll } from "./routes/get-poll";
+import { voteOnPoll } from "./routes/vote-on-poll";
 
 const app = fastify();
 
@@ -12,6 +13,7 @@ app.register(cookie, {
 
 app.register(createPoll);
 app.register(getPoll)
+app.register(voteOnPoll)
 
 app.listen({ port: 3333 }).then(() => {
   console.log("HTTP server running!");
