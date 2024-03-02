794196d63aa2aa57e7023e0945fb42533711bc6a
Matheus <ymatheus.h@gmail.com>
2024-03-01 21:27:43 -0300

    fix: :bug: Update cookie settings in voteOnPoll route

--- src/http/routes/vote-on-poll.ts
+++ src/http/routes/vote-on-poll.ts
@@ -55,8 +55,6 @@ export async function voteOnPoll(app: FastifyInstance) {
         maxAge: 60 * 60 * 24 * 30, // 30 days
         signed: true,
         httpOnly: true,
-        secure: true,
-        sameSite: 'none'
       })
     }
 
